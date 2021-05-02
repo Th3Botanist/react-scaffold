f35841821ca5f7a4849ea9d603b45d59df17d2c7
Brendan Shields <brendan.m.j.shields@gmail.com>
2021-05-02 12:21:55 +1000

    Convert js to ts
    Converting existing scaffold js files to type script.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactElement } from 'react';
 
-function App() {
+export const App = (): ReactElement => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
